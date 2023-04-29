623062ac1020546f53c0464e66119120d68ca10d
Bilgesu Cevik <45641881+bcevik@users.noreply.github.com>
2023-04-28 20:50:04 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+ 
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWrist = " + leftWristX +" rightWristX = "+ rightWristX + "difference = " + difference);
+    
+    }
+}
+
+function draw() {
+    background('#6C91C2')
+    textSize(difference);
+    fill('#FFE787');
+    text('Peter', 50, 400);
